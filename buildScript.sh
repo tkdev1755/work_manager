d22228853c339888b574db3a5e23d44fb7daeba6
tkdev1755 <khetibh@gmail.com>
2025-09-18 18:41:41 +0200

    Updated build script to support windows hosts

--- buildScript.sh
+++ buildScript.sh
@@ -4,10 +4,13 @@ OS=$2
 ARCH=$3
 # Nom de l'archive de sortie
 dart compile exe bin/work_manager.dart
-cp bin/work_manager.exe bin/work_manager
+if ! [[ "$OS" = "windows" ]]; then
+  cp bin/work_manager.exe bin/work_manager
+fi
 
 mkdir -p releases/"$VERSION"/"${OS}_$ARCH"/
 # shellcheck disable=SC2086
+
 cp bin/work_manager releases/$VERSION/"${OS}_$ARCH"/
 # shellcheck disable=SC2086
 cp buildAssets/install.sh releases/$VERSION/"${OS}_$ARCH"/
@@ -16,10 +19,9 @@ OUTPUT="workManager_${OS}_${ARCH}.zip"
 
 # Création de l'archive ZIP
 cd releases/"$VERSION"/"${OS}_$ARCH" || exit
-if [[ "$PLATFORM" = "windows" ]]; then
+if [[ "$OS" = "windows" ]]; then
     powershell.exe -Command "Compress-Archive -Path * -DestinationPath $OUTPUT -Force"
-	echo "Zipped the archive correctly"
-	ls
+	  echo "Zipped the archive correctly"
 else
 	zip -r "$OUTPUT" .
 fi
